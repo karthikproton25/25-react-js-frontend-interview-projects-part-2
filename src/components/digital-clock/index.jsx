629c7a7d75c00221e80c870ba975c8b8da19555f
Karthik <karthik2502@protonmail.com>
2024-12-06 23:24:32 +0530

    building the index.jsx file for the digital clock component

---
+++ src/components/digital-clock/index.jsx
@@ -0,0 +1,12 @@
+function DigitalClock() {
+    const [time, setTime] = useState(new Date());
+  
+    useEffect(()=>{
+   const intervalId = setInterval(()=>{
+     setTime(new Date())
+   },1000)
+  
+   return()=>{
+      clearInterval(intervalId)
+   }
+    },[])
