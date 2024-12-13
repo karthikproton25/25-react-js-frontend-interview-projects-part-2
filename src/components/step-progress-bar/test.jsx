2328d45b96018cb8922b3d0dca1d3be08df5338f
Karthik <karthik2502@protonmail.com>
2024-12-13 23:40:49 +0530

    setting up the text.jsx for the progress bar project

---
+++ src/components/step-progress-bar/test.jsx
@@ -0,0 +1,12 @@
+
+
+function StepProgressBarTest() {
+    const [activeStep, setActiveStep] = useState(0);
+    const steps = ["Step 1", "Step 2", "Step 3", "Step 4", "Step 5"];
+
+    return (
+        <div className="step-progress-bar-container">
+          <h1>Step Progress Bar</h1>
+         </div>
+
+export default StepProgressBarTest;
