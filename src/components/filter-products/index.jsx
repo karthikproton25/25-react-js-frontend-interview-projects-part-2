e2a64737b7a366b61a7d3f8a0ad6ddb70c890ba2
Karthik <karthik2502@protonmail.com>
2024-12-21 22:46:29 +0530

    editing index.jsx of the filter-products project

---
+++ src/components/filter-products/index.jsx
@@ -0,0 +1,13 @@
+function FilterProducts() {
+    const [loading, setLoading] = useState(false);
+    const [products, setProducts] = useState([]);
+
+    async function fetchProducts() {
+        try {
+
+        }
+    }
+
+return {
+    
+}
