274d05a3a8ff12622978cd031f24d01103c482c0
Karthik <karthik2502@protonmail.com>
2024-12-24 23:46:48 +0530

    finishing and testing the filter-products project

--- src/components/filter-products/index.jsx
+++ src/components/filter-products/index.jsx
@@ -4,6 +4,8 @@ import "./filter.css";
 function FilterProducts() {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
+  const [currentSelectedCategory, setCurrentSelectedCategory] = useState("");
+  const [filteredItems, setFilteredItems] = useState([]);
 
   async function fetchProducts() {
     try {
@@ -24,58 +26,67 @@ function FilterProducts() {
       console.log(e);
     }
   }
-}
 
-useEffect(() => {
-  fetchProducts();
-}, []);
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-useEffect(() => {
-  const cpyProducts = [...products];
-  setFilteredItems(
-    currentSelectedCategory !== ""
-      ? cpyProducts.filter(
-          (productItem) =>
-            productItem.category.toLowerCase() ===
-            currentSelectedCategory.toLowerCase()
-        )
-      : cpyProducts
+  useEffect(() => {
+    const cpyProducts = [...products];
+    setFilteredItems(
+      currentSelectedCategory !== ""
+        ? cpyProducts.filter(
+            (productItem) =>
+              productItem.category.toLowerCase() ===
+              currentSelectedCategory.toLowerCase()
+          )
+        : cpyProducts
+    );
+  }, [currentSelectedCategory]);
+
+  const uniqueCategories =
+    products && products.length > 0
+      ? [...new Set(products.map((productItem) => productItem.category))]
+      : [];
+
+  if (loading) {
+    return <h3>Fetching the products ! Please wait</h3>;
+  }
+
+  return (
+    <div className="filter-products-container">
+      <h1>Filter Products By Category</h1>
+      <div className="filter-categories-container">
+        {uniqueCategories.map((uniqueCategoryItem) => (
+          <button
+            onClick={() =>
+              setCurrentSelectedCategory(
+                currentSelectedCategory !== "" &&
+                  currentSelectedCategory === uniqueCategoryItem
+                  ? ""
+                  : uniqueCategoryItem
+              )
+            }
+            className={`${
+              currentSelectedCategory === uniqueCategoryItem ? "active" : ""
+            }`}
+          >
+            {uniqueCategoryItem}
+          </button>
+        ))}
+      </div>
+      <ul className="list-of-products">
+        {filteredItems && filteredItems.length > 0
+          ? filteredItems.map((productItem) => (
+              <li key={productItem.id}>
+                <p>{productItem.title}</p>
+                <button>{productItem.category}</button>
+              </li>
+            ))
+          : null}
+      </ul>
+    </div>
   );
-}, [currentSelectedCategory]);
-
-const uniqueCategories =
-products && products.length > 0
-  ? [...new Set(products.map((productItem) => productItem.category))]
-  : [];
-
-if (loading) {
-return <h3>Fetching the products ! Please wait</h3>;
 }
-
-
-return (
-  <div className="filter-products-container">
-    <h1>Filter Products By Category</h1>
-    <div className="filter-categories-container">
-      {uniqueCategories.map((uniqueCategoryItem) => (
-        <button
-          onClick={() =>
-            setCurrentSelectedCategory(
-              currentSelectedCategory !== "" &&
-                currentSelectedCategory === uniqueCategoryItem
-                ? ""
-                : uniqueCategoryItem
-            )
-          }
-          className={`${
-            currentSelectedCategory === uniqueCategoryItem ? "active" : ""
-          }`}
-
-const uniqueCategories =
-  products && products.length > 0
-    ? [...new Set(products.map((productItem) => productItem.category))]
-    : [];
-
-return {};
 
 export default FilterProducts;
