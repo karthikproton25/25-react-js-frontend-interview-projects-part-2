300032ebbae9b5f9552a905e036c919ce10c5d26
Karthik <karthik2502@protonmail.com>
2024-12-25 20:33:11 +0530

    editing index.jsx the tip-calculator project

--- src/components/tip-calculator/index.jsx
+++ src/components/tip-calculator/index.jsx
@@ -2,7 +2,18 @@ function TipCalculator() {
   return (
     <div className="tip-calculator">
       <h1>TipCalculator</h1>
-      <div className="input-container"></div>
+      <div className="input-container">
+        <label>Bill Amount:</label>
+        <input type="number" />
+      </div>
+      <div className="input-container">
+        <label>Tip Percentage:</label>
+        <input type="number" />
+      </div>
+      <div className="input-container">
+        <label>Number Of People:</label>
+        <input type="number" />
+      </div>
     </div>
   );
 }
