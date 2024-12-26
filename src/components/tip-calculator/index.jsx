7b84529ba29b2ac7f67634435950c6f729ab057b
Karthik <karthik2502@protonmail.com>
2024-12-26 23:18:53 +0530

    editing index.jsx the tip-calculator project

--- src/components/tip-calculator/index.jsx
+++ src/components/tip-calculator/index.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 function TipCalculator() {
+  const [billAmount, setBillAmount] = useState(null);
+  const [tipPercentage, setTipPercentage] = useState(10);
+  const [splitCount, setSplitCount] = useState(1);
+  const [tipAmount, setAmount] = useState(0);
+
   return (
     <div className="tip-calculator">
       <h1>TipCalculator</h1>
@@ -14,6 +21,7 @@ function TipCalculator() {
         <label>Number Of People:</label>
         <input type="number" />
       </div>
+      <button>Calculate Tip</button>
     </div>
   );
 }
