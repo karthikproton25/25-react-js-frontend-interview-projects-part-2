ec17c5c98b5dc14d9ef6355deac9d2097f881cec
Karthik <karthik2502@protonmail.com>
2024-12-29 23:43:32 +0530

    editing the index.jsx for the music-player project

---
+++ src/components/music-player/index.jsx
@@ -0,0 +1,12 @@
+const tracks = [
+  {
+    title: "Track 1",
+    source: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    title: "Track 2",
+    source: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+    image: "https://via.placeholder.com/150",
+  },
+];
