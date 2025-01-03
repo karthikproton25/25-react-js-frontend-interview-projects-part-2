17f8408a7c249dacc76841cc28da9d05edffed43
Karthik <karthik2502@protonmail.com>
2025-01-03 20:08:24 +0530

    setting up the prgress-bar project

---
+++ src/components/progress-bar/index.jsx
@@ -0,0 +1,11 @@
+function ProgressBar() {
+  return (
+    <div className="progress-bar-container">
+      <h1>Custom Progress Bar</h1>
+      <div className="progress-bar">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  );
+}
+export default ProgressBar;
