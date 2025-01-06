5e8c6b7d2466d9949831750eefb5c9cd7428ff49
Karthik <karthik2502@protonmail.com>
2025-01-06 23:17:43 +0530

    editing the index.js file for the bmi-calculator project

--- src/components/bmi-calculator/index.jsx
+++ src/components/bmi-calculator/index.jsx
@@ -2,6 +2,22 @@ function BMICalculator() {
   return (
     <div className="bmi-class-container">
       <h1>BMI Calculator</h1>
+      <div className="input-conatiner">
+        <label>Height (cm):</label>
+        <input
+          onChange={(event) => setHeight(event.target.value)}
+          type="number"
+          value={height}
+        />
+      </div>
+      <div className="input-container">
+        <label>Weight (kg):</label>
+        <input
+          onChange={(event) => setWeight(event.target.value)}
+          type="number"
+          value={weight}
+        />
+      </div>
     </div>
   );
 }
