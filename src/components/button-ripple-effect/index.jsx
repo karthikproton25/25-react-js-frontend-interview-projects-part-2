78e1739a79037626e4bb0d7f489176c814912a45
Karthik <karthik2502@protonmail.com>
2025-01-14 00:00:48 +0530

    editing index.js(function button ripple effect) for the button-ripple-effect-project

--- src/components/button-ripple-effect/index.jsx
+++ src/components/button-ripple-effect/index.jsx
@@ -1,7 +1,11 @@
 function ButtonRippleEffect() {
+    const [isRipplingEffect, setIsRipplingEffect] = useState(false);
+    const [coordinates, setCoordinates] = useState({ x: -1, y: -1 });
+
   return (
     <div className="ripple-effect-container">
       <h1>Button Ripple Effect</h1>
+      <button>Click button to see ripple effect</button>
     </div>
   );
 }
