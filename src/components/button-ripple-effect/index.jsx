a7fa8a96b96b19de4c7045ae5c4c605a5c0b5a6d
Karthik <karthik2502@protonmail.com>
2025-01-13 23:53:49 +0530

    editing index.js for the button-ripple-effect-project

---
+++ src/components/button-ripple-effect/index.jsx
@@ -0,0 +1,9 @@
+function ButtonRippleEffect() {
+  return (
+    <div className="ripple-effect-container">
+      <h1>Button Ripple Effect</h1>
+    </div>
+  );
+}
+
+export defualt ButtonRippleEffect;
