5fafb07018dc0ec399ad52a862106f335d523331
Karthik <karthik2502@protonmail.com>
2025-01-16 22:10:19 +0530

    editing the index.js(function DragAndDropFeature) for the drag and drop project

--- src/components/drag-and-drop/index.jsx
+++ src/components/drag-and-drop/index.jsx
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
+
 function DragAndDropFeature() {
+  const [loading, setLoading] = useState(false);
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchListOfTodos();
+  }, []);
+
+  console.log(todos);
+
   return (
     <div className="drag-and-drop-container">
       <h1></h1>
