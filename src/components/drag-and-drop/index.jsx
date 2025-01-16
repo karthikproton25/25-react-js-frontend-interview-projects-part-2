c4e0c85f28a49d62f270ecb6af7181edbe5f5980
Karthik <karthik2502@protonmail.com>
2025-01-16 22:12:54 +0530

    editing the index.js(updating async function) for the drag and drop project

--- src/components/drag-and-drop/index.jsx
+++ src/components/drag-and-drop/index.jsx
@@ -4,6 +4,28 @@ function DragAndDropFeature() {
   const [loading, setLoading] = useState(false);
   const [todos, setTodos] = useState([]);
 
+  async function fetchListOfTodos() {
+    try {
+      setLoading(true);
+      const apiResponse = await fetch(
+        "https://dummyjson.com/todos?limit=5&skip=0"
+      );
+      const result = await apiResponse.json();
+
+      if (result && result.todos && result.todos.length > 0) {
+        setLoading(false);
+        const updatedTodos = result.todos.map((todoItem) => ({
+          ...todoItem,
+          status: "wip",
+        }));
+        setTodos(updatedTodos);
+      }
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+    }
+  }
+
   useEffect(() => {
     fetchListOfTodos();
   }, []);
