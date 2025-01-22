ca472c2ec9586614ad686810051390c12075af4d
Karthik <karthik2502@protonmail.com>
2025-01-22 23:13:38 +0530

    edititng the index.jsx for the simple-form-validation project

--- src/components/simple-form-validation/index.jsx
+++ src/components/simple-form-validation/index.jsx
@@ -2,6 +2,16 @@ function FormValidation() {
   return (
     <div>
       <h1>Simple Form Validation</h1>
+      <form>
+        <div className="input-wrapper">
+          <label>User Name</label>
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter your username"
+          />
+        </div>
+      </form>
     </div>
   );
 }
