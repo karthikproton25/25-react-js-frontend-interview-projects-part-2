23a498406d6278302bfdbd5929834380bdb0726b
Karthik <karthik2502@protonmail.com>
2025-01-22 23:19:29 +0530

    edititng the index.jsx for the simple-form-validation project

--- src/components/simple-form-validation/index.jsx
+++ src/components/simple-form-validation/index.jsx
@@ -29,6 +29,7 @@ function FormValidation() {
             placeholder="Enter your password"
           />
         </div>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
