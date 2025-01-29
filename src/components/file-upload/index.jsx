0aa7ce16003b4a1fcdea6a8c43363a3f3510e8c5
Karthik <karthik2502@protonmail.com>
2025-01-29 23:22:12 +0530

    editing index.jsx for the file-upload project

--- src/components/file-upload/index.jsx
+++ src/components/file-upload/index.jsx
@@ -1,5 +1,10 @@
 function FileUpload() {
-  return <div className="file-upload-container"></div>;
+  return <div className="file-upload-container">
+    <h1>File Upload</h1>
+    <input type="file" name="file" reference="{UploadReference}" />
+    <label htmlFor=""></label>
+    <img src="" alt="" />
+  </div>;
 }
 
 eport default FileUpload;
