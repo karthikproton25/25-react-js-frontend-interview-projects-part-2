1983ddb01335bd85ba6616c98bdcfd662826d12d
Karthik <karthik2502@protonmail.com>
2025-01-29 23:16:01 +0530

    editing app.js for the file-upload project

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import BMICalculator from "./components/12. bmi-calculator";
 import ButtonRippleEffect from "./components/13. button-ripple-effect";
 import DragAndDropFeature from "./components/14. drag-and-drop";
 import FormValidation from "./components/15. simple-form-validation";
+import FileUpload from "./components/16. file-upload";
 
 import "./App.css";
 
@@ -35,6 +36,7 @@ function App() {
       <ButtonRippleEffect />
       <DragAndDropFeature />
       <FormValidation />
+      <FileUpload />
     </div>
   );
 }
