4c4a008fa7d749f4c353d53e71908706a3ba483e
Karthik <karthik2502@protonmail.com>
2025-01-30 22:25:21 +0530

    editing index.jsx for the file-upload project

--- src/components/file-upload/index.jsx
+++ src/components/file-upload/index.jsx
@@ -1,8 +1,25 @@
+import { useRef, useState } from "react";
+
 function FileUpload() {
     const [file, setFile] = useState();
     const uploadReference = useRef();
     const progressReference = useRef();
     const statusReference = useRef();
+
+    function handleUploadFile() {
+      const file = uploadReference.current.files[0];
+      setFile(URL.createObjectURL(file));
+      let formData = new FormData();
+      formData.append("image", file);
+      let xhr = new XMLHttpRequest();
+      xhr.upload.addEventListener("progress", handleProgress, false);
+      xhr.addEventListener("load", handleSuccess, false);
+      xhr.addEventListener("error", handleError, false);
+      xhr.addEventListener("abort", handleAbort, false);
+  
+      xhr.open("POST", "https://v2.convertapi.com/upload");
+      xhr.send(formData);
+    }
 
   return <div className="file-upload-container">
     <h1>File Upload</h1>
