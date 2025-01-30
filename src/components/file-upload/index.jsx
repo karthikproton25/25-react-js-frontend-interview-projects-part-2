1c3516789778d33e423969934aa81d3afe44b9a4
Karthik <karthik2502@protonmail.com>
2025-01-30 22:23:15 +0530

    editing index.jsx for the file-upload project

--- src/components/file-upload/index.jsx
+++ src/components/file-upload/index.jsx
@@ -2,11 +2,13 @@ function FileUpload() {
     const [file, setFile] = useState();
     const uploadReference = useRef();
     const progressReference = useRef();
+    const statusReference = useRef();
 
   return <div className="file-upload-container">
     <h1>File Upload</h1>
     <input type="file" name="file" reference="{UploadReference}" />
     <label htmlFor=""></label>
+    <p></p>
     <img src="" alt="" />
   </div>;
 }
