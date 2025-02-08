299f34bf58da20b27cf6a10961a1859bbef43a1d
Karthik <karthik2502@protonmail.com>
2025-02-08 23:03:55 +0530

    editing index.jsx for the nested-comments project

--- src/components/nested-comments/index.jsx
+++ src/components/nested-comments/index.jsx
@@ -1,4 +1,28 @@
 function NestedComments() {
+  const [inputValue, setInputValue] = useState("");
+  const [comments, setComments] = useState([
+    {
+      id: 1,
+      title: "This is first comment",
+      children: [
+        {
+          id: 2,
+          title: "This is child comment one",
+          children: [],
+        },
+        {
+          id: 3,
+          title: "This is child comment two",
+          children: [],
+        },
+        {
+          id: 4,
+          title: "This is child comment three",
+          children: [],
+        },
+      ],
+    },
+  ]);
   return (
     <div className="nested-comments-container">
       <h1>Nested Comments</h1>
