0fa4baef51b4ccb545ee79915f1c34609e22c480
Karthik <karthik2502@protonmail.com>
2025-02-11 23:21:01 +0530

    editing comment.jsx for the nested-comments project

--- src/components/nested-comments/comments.jsx
+++ src/components/nested-comments/comments.jsx
@@ -1,7 +1,14 @@
 function Comment({ comment, key }) {
   const [reply, setReply] = useState("");
   const [showReplyCommentBox, setShowReplyCommentBox] = useState(false);
-  return <li key={key}>{comment.title}</li>;
+  return (
+    <li key={key}>
+      <span>{comment.title}</span>
+      {!showReplyCommentBox ? (
+        <button onClick={() => setShowReplyCommentBox(true)}>Add Reply</button>
+      ) : null}
+    </li>
+  );
 }
 
 export default Comment;
