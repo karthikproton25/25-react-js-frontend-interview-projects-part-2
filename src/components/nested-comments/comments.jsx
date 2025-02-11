70b96c1d57825b0bb0d06a9e6d91537ed673195c
Karthik <karthik2502@protonmail.com>
2025-02-11 23:18:26 +0530

    editing comment.jsx for the nested-comments project

--- src/components/nested-comments/comments.jsx
+++ src/components/nested-comments/comments.jsx
@@ -1,4 +1,6 @@
 function Comment({ comment, key }) {
+  const [reply, setReply] = useState("");
+  const [showReplyCommentBox, setShowReplyCommentBox] = useState(false);
   return <li key={key}>{comment.title}</li>;
 }
 
