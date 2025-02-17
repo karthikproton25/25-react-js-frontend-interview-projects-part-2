eb98bee8389e6b218c387423dfdb04bfd8edf533
Karthik <karthik2502@protonmail.com>
2025-02-17 23:40:41 +0530

    editing app.js for the pdfviewer project

--- src/App.js
+++ src/App.js
@@ -15,8 +15,10 @@ import DragAndDropFeature from "./components/14. drag-and-drop";
 import FormValidation from "./components/15. simple-form-validation";
 import FileUpload from "./components/16. file-upload";
 import Quiz from "./components/17. quiz-app";
+import PdfViewer from "./components/19. pdf-viewer";
 
 import "./App.css";
+import PDFViewer from "./components/pdf-viewer";
 
 function App() {
   return (
@@ -39,6 +41,7 @@ function App() {
       <FormValidation />
       <FileUpload />
       <Quiz />
+      <PDFViewer />
     </div>
   );
 }
