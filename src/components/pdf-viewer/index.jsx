9ba40f7db5db6f78978d75adc1147d8e2a22c080
Karthik <karthik2502@protonmail.com>
2025-02-17 23:43:24 +0530

    editing the index.jsx for the pdf viewer project

--- src/components/pdf-viewer/index.jsx
+++ src/components/pdf-viewer/index.jsx
@@ -2,6 +2,17 @@ function PDFViewer() {
   const [products, setProducts] = useState([]);
   const [productDetails, setProductDetails] = useState(null);
 
+  async function fetchListOfProducts() {
+    const apiResponse = await fetch(
+      "https://dummyjson.com/products?limit=10&skip=0"
+    );
+    const result = await apiResponse.json();
+
+    if (result && result.products && result.products.length) {
+      setProducts(result.products);
+    }
+  }
+
   return <div>PDF Viewer</div>;
 }
 
