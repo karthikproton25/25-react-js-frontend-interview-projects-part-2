7beb392be3cd0eb2a4a6bc58a5cc8f5b1936bb0a
Karthik <karthik2502@protonmail.com>
2025-02-25 11:41:57 +0530

    editing index.jsx for the firebase todo project

--- src/components/firebase-todo/index.jsx
+++ src/components/firebase-todo/index.jsx
@@ -1,7 +1,19 @@
+import { Button, TextField } from "@mui/material";
+
 function FirebaseTodo() {
+  const [inputValue, setInputValue] = useState("");
+
   return (
     <div className="firebase-todo-wrappper">
       <h1>Firebase Todo Application</h1>
+      <form>
+        <TextField
+          id="todo"
+          label="Create Todo"
+          variant="outlined"
+          size="small"
+        />
+      </form>
     </div>
   );
 }
