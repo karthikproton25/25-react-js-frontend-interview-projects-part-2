3dff3e99218141dd54bed780fe0e19fe86345d6a
Karthik <karthik2502@protonmail.com>
2025-02-25 10:50:28 +0530

    setting up firebase

---
+++ src/firebase-config/index.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDenjytWyiE6vUIOP2gWSSprC0_yTbuB0c",
+  authDomain: "react-interview-question-a13b8.firebaseapp.com",
+  projectId: "react-interview-question-a13b8",
+  storageBucket: "react-interview-question-a13b8.firebasestorage.app",
+  messagingSenderId: "837818667791",
+  appId: "1:837818667791:web:bf620c2cfbcfa61294e47a",
+  measurementId: "G-SKFE1WEWQL",
+};
