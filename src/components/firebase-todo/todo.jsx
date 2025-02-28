64d86788f78744517c43e0755c373b57e0851b8b
Karthik <karthik2502@protonmail.com>
2025-02-28 21:59:48 +0530

    editing todo.jsx for the firebase todo project

--- src/components/firebase-todo/todo.jsx
+++ src/components/firebase-todo/todo.jsx
@@ -1,31 +1,38 @@
 import { Button, List, ListItem, ListItemText } from "@mui/material";
+import { deleteDoc, doc } from "firebase/firestore";
+import { db } from "../../firebase-config";
 
-function Todo({ todoItem }) {
+function Todo({ todoItem, setCurrentEditedTodoID, setInputValue, authInfo }) {
   function handleDelete(getCurrentTodoID) {
     deleteDoc(doc(db, "todos", getCurrentTodoID));
   }
+
   return (
     <List>
       <ListItem>
         <ListItemText primary={todoItem?.todoItem?.todo} />
       </ListItem>
-      <Button
-        onClick={() => handleDelete(todoItem.id)}
-        variant="contained"
-        color="secondary"
-      >
-        Delete
-      </Button>
-      <Button
-        onClick={() => {
-          setInputValue(todoItem.todoItem.todo);
-          setCurrentEditedTodoID(todoItem.id);
-        }}
-        variant="contained"
-        color="info"
-      >
-        Edit
-      </Button>
+      {authInfo !== null ? (
+        <Button
+          onClick={() => handleDelete(todoItem.id)}
+          variant="contained"
+          color="secondary"
+        >
+          Delete
+        </Button>
+      ) : null}
+      {authInfo !== null ? (
+        <Button
+          onClick={() => {
+            setInputValue(todoItem.todoItem.todo);
+            setCurrentEditedTodoID(todoItem.id);
+          }}
+          variant="contained"
+          color="info"
+        >
+          Edit
+        </Button>
+      ) : null}
     </List>
   );
 }
