196fb8f2616c7cbd9e4a46e6aaac4511c9b90dfe
Karthik <karthik2502@protonmail.com>
2025-03-19 20:41:49 +0530

    editing auth-page.jsx for the firebase-auth project

--- src/components/firebase-auth/index.jsx
+++ src/components/firebase-auth/index.jsx
@@ -1,7 +1,12 @@
+import AuthPage from "./auth-page";
+import UnauthPage from "./unauth-page";
+
 function FirebaseAuth() {
   return (
     <div>
       <h1>Firebase Auth</h1>
+      <UnauthPage />
+      <AuthPage />
     </div>
   );
 }
