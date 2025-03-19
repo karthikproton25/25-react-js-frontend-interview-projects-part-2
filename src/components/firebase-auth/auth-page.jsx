feae9ff6fe3da6170aab3610f278aad73bda217b
Karthik <karthik2502@protonmail.com>
2025-03-19 20:36:58 +0530

    editing auth-page.jsx for the firebase-auth project

--- src/components/firebase-auth/auth-page.jsx
+++ src/components/firebase-auth/auth-page.jsx
@@ -1,8 +1,9 @@
 function UnauthPage {
     return (
-        <div>
-            <div>
+        <div className="unauth-page-wrapper">
+            <div className="unauth-tab-view-container">
                 <button>Register View</button>
+                <button>Login View</button>
             </div>
         </div>
     )
