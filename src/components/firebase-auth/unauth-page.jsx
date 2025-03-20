657603c8ac57933d5a498e8be6033d78a76aca44
Karthik <karthik2502@protonmail.com>
2025-03-20 19:51:55 +0530

    editing unauth-page.jsx for the firebase-auth project

--- src/components/firebase-auth/unauth-page.jsx
+++ src/components/firebase-auth/unauth-page.jsx
@@ -1,3 +1,14 @@
+import { useState } from "react";
+
+function Registration(){
+return <div className="register"></div>
+}
+
+function Login(){
+return <div className="login"></div>
+}
+
+
 function UnauthPage {
     const [isLoginView, setIsLoginView] = useState(false);
     return (
@@ -6,6 +17,9 @@ function UnauthPage {
             <button onClick={() => setIsLoginView(false)}>Register View</button>
             <button onClick={() => setIsLoginView(true)}>Login View</button>
             </div>
+            {
+                isLoginView ? <Login/> : <Registration/>
+            }
         </div>
     )
 }
