1048bcfe962787f18bdf9f4040d8f4fe55c1b2d1
Karthik <karthik2502@protonmail.com>
2025-03-24 21:36:13 +0530

    editing index.jsx for the firebase-auth project

--- src/components/firebase-auth/index.jsx
+++ src/components/firebase-auth/index.jsx
@@ -1,12 +1,26 @@
+import { auth } from "../../firebase-config";
 import AuthPage from "./auth-page";
 import UnauthPage from "./unauth-page";
+import { useAuthState } from "react-firebase-hooks/auth";
+import "./firebase-auth.css";
 
 function FirebaseAuth() {
+  const [user, loading, error] = useAuthState(auth);
+
+  console.log(user, error, loading, "raj");
+
+  const content = loading ? (
+    <h1>Loading! Please wait</h1>
+  ) : user ? (
+    <AuthPage />
+  ) : (
+    <UnauthPage />
+  );
+
   return (
-    <div>
+    <div className="firebase-auth-container">
       <h1>Firebase Auth</h1>
-      <UnauthPage />
-      <AuthPage />
+      {content}
     </div>
   );
 }
