08756681507a426ce4e3e0c85c1b53ba6db87ead
Karthik <karthik2502@protonmail.com>
2025-03-25 21:28:38 +0530

    editing auth-page.jsx for the firebase-auth project

--- src/components/firebase-auth/auth-page.jsx
+++ src/components/firebase-auth/auth-page.jsx
@@ -4,6 +4,18 @@ import { auth, db, logout } from "../../firebase-config";
 
 function AuthPage {
     const [user, loading, error] = useAuthState(auth);
+    
+    async function fetchUserDetails() {
+        try {
+          const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+          const extractCurrentDocument = await getDocs(q);
+          const data = extractCurrentDocument.docs[0].data();
+    
+          setUserInfo(data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
 
     useEffect(() => {
         if (user) fetchUserDetails();
