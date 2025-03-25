026309d8a0b7280bfe2ce8832f1110fc2dcf0a4a
Karthik <karthik2502@protonmail.com>
2025-03-25 21:25:36 +0530

    editing auth-page.jsx for the firebase-auth project

--- src/components/firebase-auth/auth-page.jsx
+++ src/components/firebase-auth/auth-page.jsx
@@ -1,7 +1,14 @@
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db, logout } from "../../firebase-config";
+
+
 function AuthPage {
     return (
-        <div><h1>Auth Page</h1></div>
+        <div>
+            <h1>Auth Page</h1>
+        <button onClick={logout}>Logout</button>
+        </div>
     )
 }
 
-export default AuthPage;
+export default AuthPage; 
