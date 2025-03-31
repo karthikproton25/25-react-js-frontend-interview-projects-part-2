8b43ab1d5d183fcbf318dd7a3e60dfc95e1c6f81
Karthik <karthik2502@protonmail.com>
2025-03-31 19:53:30 +0530

    editing index.jsx for the debounceapi call project

--- src/components/debounce-api-call/index.jsx
+++ src/components/debounce-api-call/index.jsx
@@ -1,7 +1,17 @@
 function DebounceApiCall() {
-    return {
-        <div><h1>Debounce API Call</h1></div>
-    }
+  const [searchParam, setSearchParam] = useState("");
+  return (
+    <div>
+      <h1>Debounce API Call</h1>
+      <div className="search-wrapper">
+        <input
+          type="text"
+          value={searchparams}
+          onChange={(event) => setSearchParam(event.target.value)}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default DebounceApiCall;
